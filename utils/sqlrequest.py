10c5288538735283c2722e91b1f5150922cc0116
Csaba Botos <botcs@users.noreply.github.com>
2018-06-13 02:22:04 +0200

    Revert KiBu stuff

--- utils/sqlrequest.py
+++ utils/sqlrequest.py
@@ -51,38 +51,3 @@ def db_query(query,args=None,as_dict=True,verbose=True):
     
     print('Query took %3.2f sec' % (end-start))
     return result
-
-def push_images(photo_array,thumbnail_array,timestamps,IDs):
-    def numpy2bytes(img):
-        return cv2.imencode('.jpg', img)[1].tobytes()
-    
-    start = time.time()
-    photos = [numpy2bytes(img) for img in photo_array]
-    thumbnails = [numpy2bytes(img) for img in humbnail_array]
-    connection = pymysql.connect(host,user,password,database,charset='utf8mb4')
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT Auto_increment FROM information_schema.tables WHERE table_name=photo')
-            first_ID = cursor.fetch()[0] + 1
-            photo_IDs = list(range(first_ID, first_ID+len(photos)))
-            query = 'INSERT INTO photo (photo_ID,full_frame,timestamp) VALUES (%s,%s,%s)'
-            cursor.executemany(query,zip(photo_IDs,photos,timestamps))
-            
-            cursor.execute('SELECT Auto_increment FROM information_schema.tables WHERE table_name=thumbnail')
-            first_ID = cursor.fetch()[0] + 1
-            thumbnail_IDs = list(range(first_ID, first_ID+len(photos)))
-            query = 'INSERT INTO thumbnail (thumbnail_ID,photo_ID,thumbnail_img,timestamp) VALUES (%s,%s,%s,%s)'
-            cursor.executemany(query,zip(thumbnail_IDs,photo_IDs,photos,timestamps))
-            
-            query = 'INSERT INTO annotation (thumbnail_ID,card_ID) VALUES (%s,%s)'
-            cursor.executemany(query,zip(thumbnail_IDs,card_ID))
-            
-            connection.commit()
-    finally:
-        connection.close()
-    end = time.time()
-
-    if verbose:
-        time_str = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(end))
-        print('REQUEST SUCCESS!')
-        print('Query took %3.2f sec' % (end-start))
