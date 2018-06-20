a64df5b937b0eae1a012b2dab697eaa17a6ee429
wenti <andras@brth.hu>
2018-06-20 17:16:43 +0200

    camera test script updated to use ROS

--- scripts/basic-camera-test.py
+++ scripts/basic-camera-test.py
@@ -1,26 +1,44 @@
 import numpy as np
 import cv2
 import argparse
+import sys
+
+# Ros libraries
+import roslib
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cam', type=int, default=0)
 parser.add_argument('--hd', action='store_true', help='Save in 720p if possible')
 args = parser.parse_args()
 
-cap = cv2.VideoCapture(args.cam)
-if args.hd:
-    cap.set(3, 1280)
-    cap.set(4, 720)
+class image_stream:
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def __init__(self):
+        self.bridge = CvBridge()
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/camera/image_raw", Image, self.callback,  queue_size = 1)
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+    def callback(self, ros_data):
+        cv_image = self.bridge.imgmsg_to_cv2(ros_data, "bgr8")
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('frame',cv_image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            sys.exit(0)
+
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    istream = image_stream()
+    rospy.init_node('image_stream', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down ROS Image feature detector module"
+        cv2.destroyAllWindows()
+  
+if __name__ == '__main__':
+    main(sys.argv)
