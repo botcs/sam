a3e332fe847c12ca8fdb720439fbd0a431600e1e
botcs <botos.official@gmail.com>
2018-08-30 18:04:33 +0200

    Removed video split from cron-start.sh

--- cron_start.sh
+++ cron_start.sh
@@ -8,10 +8,10 @@ xset -display ${DISPLAY} dpms force on
 
 if ! screen -list | grep -q $SCREEN_NAME; then
   echo -n "Screen is NOT running, starting... " | tee -a $START_LOG
-  date | tee -a $START_LOG
-  screen -d -L -S video_split -m ffmpeg -s 640x480 -i /dev/video0 -codec copy -f v4l2 /dev/video1 -codec copy -f v4l2 /dev/video2
-  sleep 5
-  screen -d -L -S $SCREEN_NAME -m python3 /home/nvidia/sam/main-torch0.3-compatible.py --display --cam 1 --consecutive 5 --threshold 75  --fullscreen --database weights/REALTIME-DB.tar
+  #date | tee -a $START_LOG
+  #screen -d -L -S video_split -m ffmpeg -s 640x480 -i /dev/video0 -codec copy -f v4l2 /dev/video1 -codec copy -f v4l2 /dev/video2
+  #sleep 5
+  screen -d -L -S $SCREEN_NAME -m python3 /home/nvidia/sam/client.py --display --cam 0 --fullscreen
 else
   echo -n "Screen is running, not starting another. " | tee -a $START_LOG
   date | tee -a $START_LOG
